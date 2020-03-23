81016ebe067f57f90cec5e2093ce74730b9939ac
Nino Tannio <ninotannioh@gmail.com>
2020-03-23 08:37:43 +0700

    Update APIConfig configuration

--- app/src/main/java/com/example/funrecipes/APIConfig.kt
+++ app/src/main/java/com/example/funrecipes/APIConfig.kt
@@ -2,6 +2,6 @@ package com.example.funrecipes
 
 class APIConfig (limit: String) {
 
-    public val URL_COOKING_RECORDS = "https://cooking-records.herokuapp.com/cooking_records?offset=5&limit=$limit"
+    public val URL_COOKING_RECORDS = "https://cooking-records.herokuapp.com/cooking_records?offset=0&limit=$limit"
 
 }
