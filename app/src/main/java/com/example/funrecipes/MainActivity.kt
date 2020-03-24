6612aaf8181f3945653b9cb119228dd7283b2685
Nino Tannio <ninotannioh@gmail.com>
2020-03-24 14:53:13 +0700

    Update Main Activity Try-Catch Handling

--- app/src/main/java/com/example/funrecipes/MainActivity.kt
+++ app/src/main/java/com/example/funrecipes/MainActivity.kt
@@ -8,7 +8,9 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayout
+import org.json.JSONException
 import org.json.JSONObject
+import java.lang.RuntimeException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
@@ -118,43 +120,50 @@ class MainActivity : AppCompatActivity() {
             // To stop loading bar after finishing the background process (Set: VIEW.GONE)
             findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
 
-            val jsonObj = JSONObject(result)
-            val dataRecordsArr = jsonObj.getJSONArray("cooking_records")
+            try {
+                val jsonObj = JSONObject(result)
+                if (result.isNotEmpty()) {
+                    val dataRecordsArr = jsonObj.getJSONArray("cooking_records")
 
-            for (i in 0 until dataRecordsArr.length()){
+                    for (i in 0 until dataRecordsArr.length()) {
 
-                val item = dataRecordsArr.getJSONObject(i)
+                        val item = dataRecordsArr.getJSONObject(i)
 
-                val map = HashMap<String, String>()
+                        val map = HashMap<String, String>()
 
-                // To sort the data based on Recipe Type
-                // this would be better if the API provide recipe_type param
-                if (item.getString("recipe_type") == chosenRecipeType
-                    || "all" == chosenRecipeType) {
-                    map["image_url"] = item.getString("image_url")
-                    map["recipe_type"] = item.getString("recipe_type").replace("_", " ").capitalize()
-                    map["comment"] = item.getString("comment")
-                    map["recorded_at"] = item.getString("recorded_at")
-                    dataRecords.add(map)
+                        // To sort the data based on Recipe Type
+                        // this would be better if the API provide recipe_type param
+                        if (item.getString("recipe_type") == chosenRecipeType
+                            || "all" == chosenRecipeType
+                        ) {
+                            map["image_url"] = item.getString("image_url")
+                            map["recipe_type"] = item.getString("recipe_type").replace("_", " ").capitalize()
+                            map["comment"] = item.getString("comment")
+                            map["recorded_at"] = item.getString("recorded_at")
+                            dataRecords.add(map)
+                        }
+                    }
+
+                    //
+                    val lv = findViewById<ListView>(R.id.listview)
+
+                    lv.adapter = CustomAdapter(this@MainActivity, dataRecords)
+
+                    lv.onItemClickListener =
+                        AdapterView.OnItemClickListener { _: AdapterView<*>?, _: View?, position: Int, _: Long ->
+
+                            val intent = Intent(this@MainActivity, RecipeDetailsActivity::class.java)
+                            val dr = dataRecords[position]
+                            intent.putExtra("image_url", dr.getValue("image_url"))
+                            intent.putExtra("recipe_type", dr.getValue("recipe_type"))
+                            intent.putExtra("comment", dr.getValue("comment"))
+                            intent.putExtra("recorded_at", dr.getValue("recorded_at"))
+
+                            startActivity(intent)
+                        }
                 }
-            }
-
-            //
-            val lv = findViewById<ListView>(R.id.listview)
-
-            lv.adapter = CustomAdapter(this@MainActivity, dataRecords)
-
-            lv.onItemClickListener = AdapterView.OnItemClickListener{
-                    _: AdapterView<*>?, _: View?, position: Int, _: Long ->
-
-                val intent = Intent(this@MainActivity, RecipeDetailsActivity::class.java)
-                val dr = dataRecords[position]
-                intent.putExtra("image_url", dr.getValue("image_url"))
-                intent.putExtra("recipe_type", dr.getValue("recipe_type"))
-                intent.putExtra("comment", dr.getValue("comment"))
-                intent.putExtra("recorded_at", dr.getValue("recorded_at"))
-
-                startActivity(intent)
+            }catch (e: JSONException) {
+                throw RuntimeException(e)
             }
         }
     }
